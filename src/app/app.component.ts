5ae996b6e46bf07ca51a825a3fcc7be28d0baeb5
Gustavo Esteves <gustavoesteves@directon.com.br>
2024-10-26 14:58:17 -0300

    refactor: removing and fixing modules and root component - step 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  styles: [],
-  template: `
+    selector: 'app-root',
+    styles: [],
+    template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
-    `
+    `,
+    standalone: true,
+    imports: [HeaderComponent, RouterOutlet]
 })
 export class AppComponent {
   title = 'angular-shop';
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AppModule } from './app/app.module';
+
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app/app-routing.module';
+import { AppComponent } from './app/app.component';
+import { importProvidersFrom } from '@angular/core';
 
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+bootstrapApplication(AppComponent, {
+    providers: [
+        importProvidersFrom(BrowserModule, AppRoutingModule),
+        provideHttpClient(withInterceptorsFromDi()),
+        provideNoopAnimations(),
+    ]
+})
   .catch(err => console.error(err));
